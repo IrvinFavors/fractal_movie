25c5a3d4bcdb12e81060905949cc666b830edde2
Irvin Favors <93955643+IrvinFavors@users.noreply.github.com>
2023-12-12 10:56:52 -0500

    Update timer.h

--- timer.h
+++ timer.h
@@ -25,13 +25,33 @@
 #ifndef _TIMER_H_
 #define _TIMER_H_
 
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <sys/time.h>
+#endif
 
-/* The argument now should be a double (not a pointer to a double) */
-#define GET_TIME(now) { \
-   struct timeval t; \
-   gettimeofday(&t, NULL); \
-   now = t.tv_sec + t.tv_usec/1000000.0; \
+#ifdef _WIN32
+static double frequency;
+
+static void init_frequency() {
+    LARGE_INTEGER li;
+    QueryPerformanceFrequency(&li);
+    frequency = (double)li.QuadPart;
 }
+
+#define GET_TIME(now) { \
+    LARGE_INTEGER li; \
+    if (frequency == 0) init_frequency(); \
+    QueryPerformanceCounter(&li); \
+    now = (double)li.QuadPart / frequency; \
+}
+#else
+#define GET_TIME(now) { \
+    struct timeval t; \
+    gettimeofday(&t, NULL); \
+    now = t.tv_sec + t.tv_usec / 1000000.0; \
+}
+#endif
 
 #endif
